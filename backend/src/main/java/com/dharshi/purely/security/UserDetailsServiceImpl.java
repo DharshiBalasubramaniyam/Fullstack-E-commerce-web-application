be82a6023a26b07d851b15a60f7a59a6e247242b
dharshib <Dharshib.8@gmail.com>
2024-04-27 09:52:36 +0530

    added security folder

---
+++ backend/src/main/java/com/dharshi/purely/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.dharshi.purely.security;
+
+import com.dharshi.purely.modals.User;
+import com.dharshi.purely.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
