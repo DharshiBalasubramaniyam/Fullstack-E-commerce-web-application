8511fb7deab6bdb6105b01767719cc4ee8fe38ff
dharshib <Dharshib.8@gmail.com>
2024-04-27 09:51:05 +0530

    added handlers for role, user related custom exceptions

---
+++ backend/src/main/java/com/dharshi/purely/exceptionHandlers/RestExceptionHandler.java
@@ -0,0 +1,74 @@
+package com.dharshi.purely.exceptionHandlers;
+
+import com.dharshi.purely.dtos.reponses.ApiResponseDto;
+import com.dharshi.purely.exceptions.ServiceLogicException;
+import com.dharshi.purely.exceptions.UserAlreadyExistsException;
+import com.dharshi.purely.exceptions.UserNotFoundException;
+import com.dharshi.purely.exceptions.UserVerificationFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserNotFoundExceptionHandler(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ApiResponseDto.builder()
+                        .isSuccess(false)
+                        .message(exception.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistsException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserAlreadyExistsExceptionHandler(UserAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                ApiResponseDto.builder()
+                        .isSuccess(false)
+                        .message(exception.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(value = ServiceLogicException.class)
+    public ResponseEntity<ApiResponseDto<?>> ServiceLogicExceptionHandler(ServiceLogicException exception) {
+        return ResponseEntity.badRequest().body(
+                ApiResponseDto.builder()
+                        .isSuccess(false)
+                        .message(exception.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(value = UserVerificationFailedException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserVerificationFailedExceptionHandler(UserVerificationFailedException exception) {
+        return ResponseEntity.badRequest().body(
+                ApiResponseDto.builder()
+                        .isSuccess(false)
+                        .message(exception.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDto<?>> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
+
+        List<String> errorMessage = new ArrayList<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMessage.add(error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(
+                ApiResponseDto.builder()
+                        .isSuccess(false)
+                        .message(errorMessage.toString())
+                        .build()
+        );
+    }
+}
