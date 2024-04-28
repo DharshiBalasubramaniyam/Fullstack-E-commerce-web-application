117f73184634111cd66e4440b8c221ce8591076a
dharshib <Dharshib.8@gmail.com>
2024-04-28 13:37:13 +0530

    added order confirmation email sending method

--- backend/src/main/java/com/dharshi/purely/services/NotificationService.java
+++ backend/src/main/java/com/dharshi/purely/services/NotificationService.java
@@ -1,10 +1,12 @@
 package com.dharshi.purely.services;
 
+import com.dharshi.purely.modals.Order;
 import com.dharshi.purely.modals.User;
 import jakarta.mail.MessagingException;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Set;
 
 @Service
 public interface NotificationService {
@@ -13,4 +15,5 @@ public interface NotificationService {
 
     void sendForgotPasswordVerificationEmail(User user) throws MessagingException, UnsupportedEncodingException;
 
+    void sendOrderConfirmationEmail(User user, Order order) throws MessagingException, UnsupportedEncodingException;
 }
--- backend/src/main/java/com/dharshi/purely/services/impls/EmailNotificationService.java
+++ backend/src/main/java/com/dharshi/purely/services/impls/EmailNotificationService.java
@@ -1,16 +1,19 @@
 package com.dharshi.purely.services.impls;
 
-import com.dharshi.purely.modals.User;
+import com.dharshi.purely.modals.*;
+import com.dharshi.purely.repositories.ProductRepository;
 import com.dharshi.purely.services.NotificationService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Set;
 
 @Component
 public class EmailNotificationService implements NotificationService {
@@ -21,11 +24,14 @@ public class EmailNotificationService implements NotificationService {
     @Value("${spring.mail.username}")
     private String fromMail;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void sendUserRegistrationVerificationEmail(User user) throws MessagingException, UnsupportedEncodingException {
         String toAddress = user.getEmail();
         String fromAddress = fromMail;
-        String senderName = "Company";
+        String senderName = "Purely";
         String subject = "Please verify your registration";
         String content = "Dear " + user.getUsername() + ",<br><br>"
                 + "<p>Thank you for joining us! We are glad to have you on board.</p><br>"
@@ -33,7 +39,7 @@ public class EmailNotificationService implements NotificationService {
                 + "<p>verification code: <strong>" + user.getVerificationCode() + "</strong></p><br>"
                 + "<p><strong>Please note that the above verification code will be expired within 15 minutes.</strong></p>"
                 + "<br>Thank you,<br>"
-                + "Your company name.";
+                + "Purely.";
 
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
@@ -51,14 +57,14 @@ public class EmailNotificationService implements NotificationService {
     public void sendForgotPasswordVerificationEmail(User user) throws MessagingException, UnsupportedEncodingException {
         String toAddress = user.getEmail();
         String fromAddress = fromMail;
-        String senderName = "Company";
+        String senderName = "Purely";
         String subject = "Forgot password - Please verify your Account";
         String content = "Dear " + user.getUsername() + ",<br><br>"
                 + "<p>To change your password, enter the verification code in your device.</p><br>"
                 + "<p>verification code: <strong>" + user.getVerificationCode() + "</strong></p><br>"
                 + "<p><strong>Please note that the above verification code will be expired within 15 minutes.</strong></p>"
                 + "<br>Thank you,<br>"
-                + "Your company name.";
+                + "Purely.";
 
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
@@ -71,4 +77,42 @@ public class EmailNotificationService implements NotificationService {
 
         javaMailSender.send(message);
     }
+
+    @Override
+    public void sendOrderConfirmationEmail(User user, Order order) throws MessagingException, UnsupportedEncodingException {
+        String toAddress = user.getEmail();
+        String fromAddress = fromMail;
+        String senderName = "Purely";
+        String subject = "Purely - Order confirmation";
+
+        StringBuilder contentBuilder = new StringBuilder("Dear " + user.getUsername() + ",<br><br>"
+                + "<h2>Thank you for your order!</h2>"
+                + "<p>Your order #" + order.getId() + " has been successfully placed!</p>"
+                + "<h3>Order summary</h3>");
+        for(CartItem orderItem: order.getOrderItems()) {
+
+            Product product = productRepository.findById(orderItem.getProduct().getId()).orElse(null);
+            String description = product.getProductName() + ": " + orderItem.getQuantity() + " x " + product.getPrice() + "<br>";
+            contentBuilder.append(description);
+        }
+
+        String content = contentBuilder.toString();
+
+        content += "<h4>Total: " + order.getOrderAmt() + "</h4>"
+                + "<p>Delivery charges be will added to your total at your doorstep!</p>"
+                + "<br>Thank you,<br>"
+                + "Purely.";
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(fromAddress, senderName);
+        helper.setTo(toAddress);
+        helper.setSubject(subject);
+
+        helper.setText(content, true);
+
+        javaMailSender.send(message);
+    }
+
 }
