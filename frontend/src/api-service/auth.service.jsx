1b4b5529773b683b6761894a9874c01c90e71ebc
dharshib <Dharshib.8@gmail.com>
2024-04-29 20:03:56 +0530

    add auth service

---
+++ frontend/src/api-service/auth.service.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react"
+import API_BASE_URL from "./apiConfig";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+function AuthService() {
+    const [isLoading, setLoading] = useState(false);
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const login = async (email, password) => {
+        setLoading(true)
+        await axios.post(`${API_BASE_URL}/auth/signin`, { email, password })
+            .then((response) => {
+                if (response.data.token) {
+                    setResponse(true);
+                    setError(null)
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                    navigate("/");
+                }
+            })
+            .catch((error) => {
+                setResponse(null)
+                const resMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+                if (resMessage == "Bad credentials") {
+                    setError("Invalid email or password!");
+                } else {
+                    setError("Unable to perform login now. Try again later!");
+                }
+            })
+        setLoading(false)
+    }
+
+    const save = async (userName, email, password) => {
+        setLoading(true)
+        await axios.post(`${API_BASE_URL}/auth/signup`, { userName, email, password })
+            .then((response) => {
+                console.log(response.data)
+                if (response.data.token) {
+                    setResponse(true);
+                    setError(null)
+                    navigate(`/auth/userRegistrationVerfication/${data.email}`);
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+                setResponse(null)
+                if (error.response) {
+                    setError(error.response.data.message);
+                } else {
+                    setError("Unable to perform register now. Try again later!");
+                }
+            })
+        setLoading(false)
+    }
+
+    const verifyRegistration = async (verificationCode) => {
+        setLoading(true)
+        await axios.get(`${API_BASE_URL}/auth/signup/verify`, {
+                params: {
+                    code: verificationCode
+                }
+            })
+            .then((response) => {
+                console.log(response.data)
+                if (response.data.token) {
+                    setResponse(true);
+                    setError(null)
+                    navigate(`/auth/success-registration`);
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+                setResponse(null)
+                if (error.response) {
+                    setError(error.response.data.message);
+                } else {
+                    setError("Unable to perform verify now. Try again later!");
+                }
+            })
+        setLoading(false)
+    }
+
+    const resendVerificationCode = async (email) => {
+        await axios.get(`${API_BASE_URL}/auth/signup/resend`, {
+                params: {
+                    email: email
+                }
+            })
+            .then((response) => {
+                console.log(response.data)
+                if (response.data.token) {
+                    setResponse(true);
+                    setError(null)
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+                setResponse(null)
+                if (error.response) {
+                    setError(error.response.data.message);
+                } else {
+                    setError("Unable to resend code now. Try again later!");
+                }
+            })
+    }
+
+    return {login, save, verifyRegistration, resendVerificationCode, isLoading, response, error};
+}
+
+export default AuthService;
