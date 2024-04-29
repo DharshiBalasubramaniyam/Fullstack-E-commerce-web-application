29fb9d25f6259de51f83ae4f1f1c7b41d4c07582
dharshib <Dharshib.8@gmail.com>
2024-04-29 20:09:03 +0530

    add products page

---
+++ frontend/src/pages/products/products.jsx
@@ -0,0 +1,124 @@
+import './products.css'
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import Header from "../../components/header/header";
+import { useContext, useEffect, useState } from 'react';
+import Loading from '../../components/loading/loading';
+import Info from '../../components/info/info';
+import ProductService from '../../api-service/product.service';
+import CartContext from '../../contexts/cart.contect';
+import { AuthContext } from '../../contexts/auth.context';
+
+function Products() {
+
+    const { category } = useParams();
+    const location = useLocation();
+    const { getAllCategories, getAllProducts, getProductsByCategory, isLoading, categories, products, error } = ProductService()
+
+    useEffect(() => {
+        getAllCategories()
+        if (location.state) {
+            getProductsByCategory(location.state.categoryId)
+        } else {
+            getAllProducts()
+        }
+    }, [category])
+
+    return (
+        <>
+            <Header />
+            {isLoading && <Loading />}
+            {error && <Info message="Unable to display product right now. Try again later..."/>}
+            {!isLoading && !error && (
+                <>
+                    <CategoryWrapper category={category} categoryList={categories} />
+                    <ProductsWrapper products={products} />
+                </>
+            )}
+        </>
+    )
+}
+
+export default Products;
+
+
+function CategoryWrapper({ category, categoryList }) {
+
+    const navigate = useNavigate();
+
+    const onSelect = (categoryName, categoryId) => {
+        categoryId ?
+            navigate(`/products/${categoryName}`, { state: { categoryId: categoryId } })
+            :
+            navigate(`/products/${categoryName}`)
+    }
+
+    return (
+        <section className="category-wrapper">
+            <div className="category-list">
+                <div
+                    className={category == "All" ? "category active" : "category"}
+                    onClick={() => onSelect("All")}
+                >
+                    All
+                </div>
+                {
+                    categoryList.map((cat) => {
+                        return (
+                            <div
+                                className={category == cat.categoryName ? "category active" : "category"}
+                                key={cat.id}
+                                onClick={() => onSelect(cat.categoryName, cat.id)}
+                            >
+                                {cat.categoryName}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </section>
+    )
+}
+
+
+function ProductsWrapper({ products }) {
+
+    const { addItemToCart } = useContext(CartContext);
+    const {user, toggleUser} = useContext(AuthContext)
+    const [isLoading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const onAddToCart = async (productId) => {
+        if (!user) {
+            navigate("/auth/login")
+            return
+        }
+        setLoading(true)
+        await addItemToCart(productId, 1)
+        setLoading(false)
+    }
+
+    return (
+        <section className='products-wrapper'>
+            {
+                isLoading ? <Loading/> :
+                products.map((product) => {
+                    return (
+                        <div className='box' key={product.id}>
+                            <img src={`../../../public/products/${product.imageUrl}`} className="image" alt='product'></img>
+                            <div className='price' aria-label='image'>Rs. {product.price}</div>
+                            <div className='text-part'>
+                                <div className='name'>{product.name}</div>
+                                <div className='description'>{product.description}</div>
+                            </div>
+                            <button
+                                onClick={() => onAddToCart(product.id)}
+                            >
+                                Add to cart
+                            </button>
+                        </div>
+                    )
+                })
+            }
+        </section>
+    )
+}
