1a94ce4b5621a909d1d955aba3b571fb5cf975cf
dharshib <Dharshib.8@gmail.com>
2024-04-29 20:07:09 +0530

    add auth context

---
+++ frontend/src/contexts/auth.context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuth = () => {
+    const [user, setUser] = useState()
+
+    const toggleUser = () => {
+        const user = JSON.parse(localStorage.getItem("user"))
+        setUser(user)
+    }
+
+    useEffect(() => {
+        toggleUser()
+    }, [])
+
+
+    return {user, toggleUser}
+}
