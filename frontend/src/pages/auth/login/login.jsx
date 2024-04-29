6bc5e9710b61f0b84526c27119231bceaa430a79
dharshib <Dharshib.8@gmail.com>
2024-04-29 20:05:20 +0530

    add login page

---
+++ frontend/src/pages/auth/login/login.jsx
@@ -0,0 +1,78 @@
+import "../auth.css"
+import {useForm} from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from "../../../components/logo/logo";
+import AuthService from "../../../api-service/auth.service";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../../contexts/auth.context";
+
+function Login() {
+
+    const {register, handleSubmit,formState} = useForm();
+    const { login, isLoading, error } = AuthService();
+    const {user} = useContext(AuthContext)
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        if (user && user.token) {
+            navigate("/");
+        }
+    }, [])
+
+    const onSubmit = async (data) => {
+        login(data.email, data.password)
+    }
+    return (
+        <main className='auth-container'>
+            <div className='auth-wrapper'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Logo />
+                    <h2>Login to continue...</h2>
+                    {
+                        error && <p>{error}</p>
+                    }
+
+                    <div className='input-box'>
+                        <label>
+                            Email
+                            <input
+                                type='text'
+                                placeholder="john@gmail.com"
+                                {...register('email', {
+                                    required: "Email is required!",
+                                    pattern: { value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, message: "Invalid email address!" }
+                                })}
+                            />
+                        </label>
+
+                        {formState.errors.email && <small>{formState.errors.email.message}</small>}
+                    </div>
+
+                    <div className='input-box'>
+                        <label>
+                            Password
+                            <input
+                                type='password'
+                                placeholder="********"
+                                {
+                                    ...register('password', {
+                                        required: 'Password is required!'
+                                })
+                                }
+                            />
+                        </label>
+                        {formState.errors.password && <small>{formState.errors.password.message}</small>}
+                    </div>
+                    <br/>
+
+                    <div className='input-box'>
+                        <input type='submit' value={isLoading ? 'Logging in' : 'Login'} className={isLoading ? 'loading' : ''} />
+                    </div>
+                    <div className='msg'>New member? <Link to='/auth/register' className='auth-link'>Register Here</Link></div>
+                </form>
+            </div>
+        </main>
+    )
+}
+
+export default Login;
