bbc6ff09f9811e00c2339a607ed05a0bbb3cce02
dharshib <Dharshib.8@gmail.com>
2024-05-01 20:03:32 +0530

    added category-service dtos

---
+++ microservice-backend/category-service/src/main/java/com/dharshi/categoryservice/dtos/CategoryRequestDto.java
@@ -0,0 +1,13 @@
+package com.dharshi.categoryservice.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryRequestDto {
+
+    private String name;
+}
