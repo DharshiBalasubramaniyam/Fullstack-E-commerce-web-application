25a2ac0387dc8063d7c1cf802ab32390869f9b60
dharshib <Dharshib.8@gmail.com>
2024-05-01 08:18:40 +0530

    renamed backend folder name to monotholic-backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter } from "react-router-dom"
+import AppRoutes from "./routes/routes"
+import './assets/styles/index.css'
+import { AuthContext, useAuth } from "./contexts/auth.context"
+import CartService from "./api-service/cart.service";
+import CartContext from "./contexts/cart.contect";
+
 function App() {
+
+  const {user, toggleUser} = useAuth();
+  const { cart, cartError, isProcessingCart, addItemToCart, removeItemFromCart, getCartInformation } = CartService();
+
   return (
-    <>
-      <div>
-        <h1>Welcome to purely!</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <AuthContext.Provider value={{user, toggleUser}}>
+        <CartContext.Provider value={{ cart, cartError, isProcessingCart, addItemToCart, removeItemFromCart, getCartInformation }}>
+          <AppRoutes/>
+        </CartContext.Provider>
+      </AuthContext.Provider>
+    </BrowserRouter>
   )
 }
 
