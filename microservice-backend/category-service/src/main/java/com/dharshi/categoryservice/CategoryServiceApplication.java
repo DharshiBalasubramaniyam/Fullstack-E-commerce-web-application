c7d9bfffdf84bf050ddbeca3ff5733808c1f3cee
dharshib <Dharshib.8@gmail.com>
2024-05-01 09:11:54 +0530

    initialized category microservice

---
+++ microservice-backend/category-service/src/main/java/com/dharshi/categoryservice/CategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dharshi.categoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryServiceApplication.class, args);
+	}
+
+}
