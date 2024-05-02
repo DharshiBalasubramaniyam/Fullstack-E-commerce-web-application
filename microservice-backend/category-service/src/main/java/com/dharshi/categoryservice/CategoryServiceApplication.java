373e5e4a226ed08abeee725ebea6d6c9aa296333
dharshib <Dharshib.8@gmail.com>
2024-05-02 20:47:57 +0530

    added @EnableDiscoveryClient

--- microservice-backend/category-service/src/main/java/com/dharshi/categoryservice/CategoryServiceApplication.java
+++ microservice-backend/category-service/src/main/java/com/dharshi/categoryservice/CategoryServiceApplication.java
@@ -2,8 +2,10 @@ package com.dharshi.categoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CategoryServiceApplication {
 
 	public static void main(String[] args) {
