bdcbb8ce0caf219a85fc532c4d3a3b73baee6102
dharshib <Dharshib.8@gmail.com>
2024-05-05 16:20:15 +0530

    added hero section

---
+++ frontend/src/components/home_hero/hero.jsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth.context';
+import './hero.css'
+import {Link} from 'react-router-dom'
+
+function Hero() {
+    
+    const {user, toggleUser} = useContext(AuthContext)
+
+    return(
+        <section className="hero-section" id='hero'>
+            {
+                (user) &&  <h4>Wecome back, {user.username}!</h4>
+            }
+            <h1>Welcome to leading health and wellness retailer.</h1>
+
+            <h3>We believe in pure, natural goodness - plain and simple. We aim to partner with you on your unique wellness journey while honoring people and the planet in all that we do.</h3>
+            <div>
+                <Link to='/products/All'><button>Shop now</button></Link>
+                
+            </div>
+        </section>
+    )
+}
+
+
+
+export default Hero;
