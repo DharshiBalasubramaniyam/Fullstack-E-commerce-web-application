f709b0bd3e53a36b6bbc1f7aeae1970a4a1bb1bc
dharshib <Dharshib.8@gmail.com>
2024-05-05 16:21:40 +0530

    updated logo

--- frontend/src/components/logo/logo.jsx
+++ frontend/src/components/logo/logo.jsx
@@ -1,6 +1,8 @@
 import './logo.css'
+import {Link} from 'react-router-dom'
+
 function Logo() {
-    return <h1 className="logo-h">Purely</h1>
+    return <Link to='/'><h1 className='logo'><span></span>Purely</h1></Link>
 }
 
 export default Logo;
