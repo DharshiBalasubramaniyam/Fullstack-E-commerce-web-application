90fe15450487f500d9ae3f20f19e3a22f588d67c
dharshib <Dharshib.8@gmail.com>
2024-05-07 14:59:05 +0530

    update order success page styles

--- frontend/src/pages/checkout/order.success.jsx
+++ frontend/src/pages/checkout/order.success.jsx
@@ -3,26 +3,23 @@ import Logo from "../../components/logo/logo";
 import { AuthContext } from "../../contexts/auth.context";
 import { useContext } from "react";
 import success from "../../assets/images/icons/success.gif"
+import './checkout.css'
 
 function OrderSuccess() {
 
-    const { user, toggleUser }= useContext(AuthContext)
+    const { user, toggleUser } = useContext(AuthContext)
 
-    return(
-        <main className='auth-container'>
-            <div className='auth-wrapper'>
-                <form style={{alignItems:'center'}}>
-                    <Logo/>
-                    <img src={success} size='20px'/>
-                    <h4 style={{textAlign:"center", color: "green"}}>
-                        Thank you for your order!<br/>
-                        Your order has been successfully placed.
-                    </h4>
-                    <h4 style={{textAlign:"center", color: "green"}}>Order confirmation email has been sent to {user.email}.</h4>
-                    {/* <div className="input-box"> */}
-                        <Link to='/'><button>Go home</button></Link>
-                    {/* </div> */}
-                </form>
+    return (
+        <main className='order-success'>
+            <div className='order-success-box'>
+                <Logo />
+                <img src={success} />
+                <h4 style={{ textAlign: "center", color: "green" }}>
+                    Thank you for your order!<br />
+                    Your order has been successfully placed.
+                </h4>
+                <h4 style={{ textAlign: "center", color: "green" }}>Order confirmation email has been sent to {user?.email}.</h4>
+                <Link to='/'><button>Go home</button></Link>
             </div>
         </main>
     )
