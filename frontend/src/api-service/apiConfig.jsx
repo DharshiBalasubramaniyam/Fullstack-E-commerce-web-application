02dfe3cab214a3cda127089256030fcfa29b7a53
dharshib <Dharshib.8@gmail.com>
2025-08-04 20:34:36 +0530

    Update API base URL configuration to use environment variable

--- frontend/src/api-service/apiConfig.jsx
+++ frontend/src/api-service/apiConfig.jsx
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://localhost:8080"
+const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"
 
 export default API_BASE_URL;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // environment variables
+  define: {
+    'process.env': {
+      VITE_API_BASE_URL: JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:8080')
+    }
+  },
 })
