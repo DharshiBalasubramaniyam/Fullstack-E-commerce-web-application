ea8391db450ea7ef369e6a26d573448d48ded025
dharshib <Dharshib.8@gmail.com>
2025-08-16 20:22:12 +0530

    Update web workflow

--- frontend/src/api-service/apiConfig.jsx
+++ frontend/src/api-service/apiConfig.jsx
@@ -1,3 +1,3 @@
-const API_BASE_URL =  "http://gateway-svc:8080" || "http://localhost:8080"
+const API_BASE_URL =  "http://a8b6f9b52d534436ba4e05b23a4c9ac9-242631453.us-east-1.elb.amazonaws.com/" || "http://localhost:8080"
 
 export default API_BASE_URL;
