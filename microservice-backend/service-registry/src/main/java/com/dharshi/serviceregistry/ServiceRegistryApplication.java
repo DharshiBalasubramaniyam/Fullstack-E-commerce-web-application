16fef14b5d0e1c8b0ecbd8b29252638852f49037
dharshib <Dharshib.8@gmail.com>
2025-08-19 07:20:57 +0530

    Update registry

--- microservice-backend/service-registry/src/main/java/com/dharshi/serviceregistry/ServiceRegistryApplication.java
+++ microservice-backend/service-registry/src/main/java/com/dharshi/serviceregistry/ServiceRegistryApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
-
 }
